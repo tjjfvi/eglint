c0aa6451d16a2ffb64919a893f6369364303d6c8
tjjfvi <tjjfvi@tjjfvi.com>
2021-07-07 14:11:30 -0700

    add no-late-call check to {Forked,Mapped}Selection#apply

--- src/Selection.ts
+++ src/Selection.ts
@@ -4,7 +4,7 @@ import { Node } from "./Node"
 
 export class Selection<T extends Node = Node> {
 
-  constructor(public _values = new Set<T>()){}
+  constructor(private _values = new Set<T>()){}
 
   first(): T | undefined{
     return this._values.values().next().value
@@ -60,12 +60,16 @@ export class Selection<T extends Node = Node> {
 
 export class ForkedSelection<Sel extends Selection<T>, T extends Node> extends Selection<T> {
 
+  private _originalSize = this._original.size
+
   constructor(private _original: Sel){
-    super(new Set(_original._values))
+    super(new Set(_original["_values"]))
   }
 
   apply(): Sel{
-    this._original._values = this._values
+    if(this._originalSize !== this._original.size)
+      throw new Error("ForkedSelection#apply called late")
+    this._original["_values"] = this["_values"]
     return this._original
   }
 
@@ -74,21 +78,24 @@ export class ForkedSelection<Sel extends Selection<T>, T extends Node> extends S
 export class MappedSelection<Sel extends Selection<T>, T extends Node, U extends Node> extends Selection<U> {
 
   private _reverseMap = new MultiMap<U, T>()
+  private _originalSize = this._original.size
 
   constructor(private _original: Sel, fn: (value: T) => Iterable<U>){
     super(new Set())
-    for(const t of this._original._values)
+    for(const t of this._original["_values"])
       for(const u of fn(t)) {
-        this._values.add(u)
+        this["_values"].add(u)
         this._reverseMap.add(u, t)
       }
   }
 
   apply(): Sel{
-    this._original._values.clear()
-    for(const u of this._values)
+    if(this._originalSize !== this._original.size)
+      throw new Error("MappedSelection#apply called late")
+    this._original["_values"].clear()
+    for(const u of this["_values"])
       for(const t of this._reverseMap.get(u))
-        this._original._values.add(t)
+        this._original["_values"].add(t)
     return this._original
   }
 
